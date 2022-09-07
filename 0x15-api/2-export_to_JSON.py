4690f61248a4b9db2f9c8a3b8cb1279bedda010e
maurolabella <maurolabella@yahoo.com>
2022-09-06 23:54:05 -0300

    a

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Script that uses a REST API to conform a JSON
+"""
+import json
+import requests
+from sys import argv
+
+
+def tr_usr_tojson(uid):
+    """GET user todos at given id"""
+    url = "https://jsonplaceholder.typicode.com/"
+    endpoint_a = "users/"
+    endpoint_b = "todos?userId="
+
+    """Employee Name"""
+    query_employee = url + endpoint_a + uid
+    employee = requests.get(query_employee).json()
+    """Employee Tasks"""
+    query_tasks = url + endpoint_b + uid
+    tasks = requests.get(query_tasks).json()
+    """Concatenate two responses"""
+    data = {"employee": employee, "tasks": tasks}
+    out = []
+    employee_id = data.get("employee").get("id")
+    for tsk in data.get("tasks"):
+        o = {}
+        o["task"] = tsk.get("title")
+        o["completed"] = tsk.get("completed")
+        o["username"] = data.get("employee").get("username")
+        out.append(o)
+    with open('{}.json'.format(uid), 'w') as json_file:
+        json.dump({employee_id: out}, json_file)
+
+
+if __name__ == '__main__':
+    """Excecute it"""
+    tr_usr_tojson(argv[1])
