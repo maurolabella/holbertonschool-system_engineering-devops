f44fa853a44dd777a37ef9dd1a8b6993349eec85
maurolabella <maurolabella@yahoo.com>
2022-09-22 11:57:39 -0300

    b

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -26,7 +26,9 @@ def count_words(subreddit, word_list, after="", counter={}, ini=0):
         if _after is not None:
             count_words(subreddit, word_list, _after, counter, 1)
         else:
-            str = sorted(counter.items(), key=lambda kv: kv[1], reverse=True)
+
+            str = sorted(counter.items(), key=lambda kv: (
+                kv[1], kv[0]), reverse=True)
             for name, num in str:
                 if num != 0:
                     print('{}: {}'.format(name, num))
